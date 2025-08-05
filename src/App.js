5245b1f153fc6964082ae98bf414f181f974aa3a
mdmurad71 <mdmurad1685@gmail.com>
2025-08-05 11:08:04 +0600

    first

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+        // <h1 className="text-3xl font-bold underline">
+        //   Hello world!
+        // </h1>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Click Me
+      </button>
   );
 }
 
